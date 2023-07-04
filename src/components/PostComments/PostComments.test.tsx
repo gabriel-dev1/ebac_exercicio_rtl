2dc7aa77532ba68d2f64ee09d7e9e6188ae75eb6
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-07-04 17:20:37 -0300

    Upload do exercício

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,42 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from '@testing-library/react'
+import PostComment from '.'
+/* import Post from '.'; */
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+
+    it('Deve registrar dois comentarios', () => {
+        render(<PostComment />)
+        /*  const { debug } = render(<PostComment />) */
+
+        fireEvent.change(screen.getByTestId('text-comentar'), {
+            target: {
+                value: 'O'
+            }
+        })
+
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        fireEvent.change(screen.getByTestId('text-comentar'), {
+            target: {
+                value: 'carro'
+            }
+        })
+
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        fireEvent.change(screen.getByTestId('text-comentar'), {
+            target: {
+                value: 'do batman'
+            }
+        })
+
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        /* debug() */
+        expect(screen.getAllByTestId('comentar-test')).toHaveLength(3)
+    })
+})
